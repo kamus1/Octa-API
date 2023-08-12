52d0af76404f1523adcb54a33da417bd37a252c7
kamus1 <benjamincamus9@gmail.com>
2023-08-12 19:38:39 -0400

    Updated accelerometer format

--- main.py
+++ main.py
@@ -1,63 +1,105 @@
 from fastapi import FastAPI
 import json
 import pandas as pd
+import os
 
-#routes windows
-'''
-csv_accelerometer_path = "data\\GY-91.csv"
-csv_temperatureHumidity_path = "data\\HDC-1080.csv"
-csv_latitudeLongitude_path = "data\\NEO-6M.csv"
-csv_octa_path ="data\\OctaCSV.csv"
-'''
+# File names
+ACCELEROMETER_FILE = "GY-91.csv"
+TEMPERATURE_HUMIDITY_FILE = "HDC-1080.csv"
+LATITUDE_LONGITUDE_FILE = "NEO-6M.csv"
+OCTA_FILE = "OctaCSV.csv"
 
-#routes linux
-csv_accelerometer_path = "data/GY-91.csv"
-csv_temperatureHumidity_path = "data/HDC-1080.csv"
-csv_latitudeLongitude_path = "data/NEO-6M.csv"
-csv_octa_path ="data/OctaCSV.csv"
-
+# Build the full paths to the files using os.path
+CSV_ACCELEROMETER_PATH = os.path.join("data", ACCELEROMETER_FILE)
+CSV_TEMPERATURE_HUMIDITY_PATH = os.path.join("data", TEMPERATURE_HUMIDITY_FILE)
+CSV_LATITUDE_LONGITUDE_PATH = os.path.join("data", LATITUDE_LONGITUDE_FILE)
+CSV_OCTA_PATH = os.path.join("data", OCTA_FILE)
 
 
 app = FastAPI()
 
-#main
+# Main
 @app.get("/")
 def index():
-    return {"message":"Octa-API"}
+    return {"message": "Octa-API"}
 
 # GY-91
 @app.get("/accelerometer")
 def accelerometer():
     # Read the CSV file using pandas
-    df = pd.read_csv(csv_accelerometer_path)
+    df = pd.read_csv(CSV_ACCELEROMETER_PATH)
     
     # Convert the data into a structured JSON format
-    json_data = df.to_json(orient="records", date_format="iso", indent=2)
+    json_data = []
+    for index, row in df.iterrows():
+        accelerometer_values = row["Acelerometer"].strip("[]").split(", ")
+        gyroscope_values = row["Gyroscope"].strip("[]").split(", ")
+        magnetometer_values = row["Magnetometer"].strip("[]").split(", ")
+        
+        x_accelerometer, y_accelerometer, z_accelerometer = map(float, accelerometer_values)
+        formatted_accelerometer = {
+            "x": x_accelerometer,
+            "y": y_accelerometer,
+            "z": z_accelerometer
+        }
+        
+        x_gyroscope, y_gyroscope, z_gyroscope = map(float, gyroscope_values)
+        formatted_gyroscope = {
+            "x": x_gyroscope,
+            "y": y_gyroscope,
+            "z": z_gyroscope
+        }
+        
+        x_magnetometer, y_magnetometer, z_magnetometer = map(float, magnetometer_values)
+        formatted_magnetometer = {
+            "x": x_magnetometer,
+            "y": y_magnetometer,
+            "z": z_magnetometer
+        }
+        
+        entry = {
+            "Time": row["Time"],
+            "Acelerometer": formatted_accelerometer,
+            "Gyroscope": formatted_gyroscope,
+            "Magnetometer": formatted_magnetometer
+        }
+        
+        json_data.append(entry)
     
     # Return the JSON as a response
-    return json.loads(json_data)  # Convert the JSON into a Python object (dict/list) before returning
+    return json_data
 
-
-#HDC-1080
+# HDC-1080
 @app.get("/temperatureHumidity")
-def accelerometer():
-    df = pd.read_csv(csv_temperatureHumidity_path)
-    json_data = df.to_json(orient="records", date_format="iso", indent=2)
+def temperature_humidity():
+    # Read the CSV file using pandas
+    df = pd.read_csv(CSV_TEMPERATURE_HUMIDITY_PATH)
     
-    return json.loads(json_data)
+    # Convert the data into a structured JSON format
+    json_data = []
+    for index, row in df.iterrows():
+        entry = {
+            "Time": row["Time"],
+            "Temperature": row["Temperature(C)"],
+            "Humidity": row["Humidity"]
+        }
+        json_data.append(entry)
+    
+    # Return the JSON as a response
+    return json_data
 
-#NEO-6M
+# NEO-6M
 @app.get("/latitudeLongitude")
-def accelerometer():
-    df = pd.read_csv(csv_latitudeLongitude_path)
+def latitude_longitude():
+    df = pd.read_csv(CSV_LATITUDE_LONGITUDE_PATH)
     json_data = df.to_json(orient="records", date_format="iso", indent=2)
     
     return json.loads(json_data)
 
-#OctaCSV
+# OctaCSV
 @app.get("/octa")
-def accelerometer():
-    df = pd.read_csv(csv_octa_path)
+def octa():
+    df = pd.read_csv(CSV_OCTA_PATH)
     json_data = df.to_json(orient="records", date_format="iso", indent=2)
     
     return json.loads(json_data)
